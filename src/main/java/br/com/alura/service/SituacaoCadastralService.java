7e5ed6981cb2e6792c3d3410a6c6ed288d183741
Gabriel Sartoretto <gabrielsartoretto@gmail.com>
2026-09-23 17:16:17 -0300

    subindo kafka

--- src/main/java/br/com/alura/service/SituacaoCadastralService.java
+++ src/main/java/br/com/alura/service/SituacaoCadastralService.java
@@ -3,9 +3,11 @@ package br.com.alura.service;
 import br.com.alura.domain.Agencia;
 import br.com.alura.domain.audit.Audit;
 import br.com.alura.repository.SituacaoCadastralRepository;
-import io.smallrye.mutiny.Uni;
-import jakarta.enterprise.context.ApplicationScoped;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.reactive.messaging.MutinyEmitter;
+import jakarta.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 
@@ -13,15 +15,25 @@ import org.eclipse.microprofile.reactive.messaging.Emitter;
 public class SituacaoCadastralService {
 
     private final SituacaoCadastralRepository situacaoCadastralRepository;
+
+    // Envio de mensagem pelo RabbitMQ
     private final Emitter<Audit> emitter;
+
+    // Envio de mensagem pelo Kafka
+    private final MutinyEmitter<String> mutinyEmitter;
+    private final ObjectMapper objectMapper;
 
 
     public SituacaoCadastralService(
             SituacaoCadastralRepository situacaoCadastralRepository,
-            @Channel("notificacoes") Emitter<Audit> emitter
+            @Channel("notificacoes") Emitter<Audit> emitter,
+            @Channel("remover-agencia-channel") MutinyEmitter<String> mutinyEmitter,
+            ObjectMapper objectMapper
     ) {
         this.situacaoCadastralRepository = situacaoCadastralRepository;
         this.emitter = emitter;
+        this.mutinyEmitter = mutinyEmitter;
+        this.objectMapper = objectMapper;
     }
 
     @WithTransaction
@@ -30,7 +42,20 @@ public class SituacaoCadastralService {
                 .update("situacaoCadastral = ?1 where cnpj = ?2",
                         agencia.getSituacaoCadastral(), agencia.getCnpj())
                 .onItem().invoke(() -> {
+                    //RabbitMQ
                     emitter.send(new Audit(agencia.getId(), agencia.getCnpj(), agencia.getSituacaoCadastral()));
+                })
+                // Kafka
+                .call(() -> {
+                    try {
+                        if (agencia.getSituacaoCadastral().equals("INATIVO")) {
+                            String agenciaJson = objectMapper.writeValueAsString(agencia);
+                            return mutinyEmitter.send(agenciaJson);
+                        }
+                        return Uni.createFrom().voidItem();
+                    } catch (Exception e) {
+                        return Uni.createFrom().failure(e);
+                    }
                 })
                 .replaceWithVoid();
     }
